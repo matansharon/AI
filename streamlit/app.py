a77891355ac87c72bc92867dda7a0f0f9df6b15c
matan <matca2953@gmail.com>
2023-05-31 23:51:01 +0300

    finish good

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,30 +4,21 @@ import os
 from dotenv import load_dotenv
 
 
-import os
-import openai
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
-
-
-def get_query(question):
+def get_response(message):
     load_dotenv()
-    key = os.getenv("openai_key")
-    openai.api_key = key
-    response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt=question,
-  temperature=0,
-  max_tokens=100,
-  top_p=1,
-  frequency_penalty=0.0,
-  presence_penalty=0.0,
-  stop=["\n", "Q:"]
-)
-    return response.choices[0].text
-    
+    openai.api_key = os.getenv("openai_key")
+    # os.environ['openai_key']
+    # key = os.getenv("openai_key")
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo',
+        temperature = 1,
+        messages = [
+            {"role": "user", "content": message}
+        ]
+    )
+    return response.choices[0]["message"]["content"]
+
+
 #create a header that will as for a question. and a button to submit the question
 st.title("Ask a question")
 question = st.text_input("What is your question?")
@@ -36,8 +27,8 @@ submit = st.button("Submit")
 #when the button is clicked, the question will be sent to the model and the answer will be returned
 if submit:
     st.write("The answer to your question is: ")
-    q=get_query(question)
+    q=get_response(question)
     st.write(q)
-    print(q)
+    
     #get an answer from openai api gpt3 and display it
     
