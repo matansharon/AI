50d9183b61b0dd07a4f075637eea1021b7f68fd7
matan <matca2953@gmail.com>
2023-05-31 23:04:13 +0300

    adding streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -11,3 +11,12 @@ st.markdown(user_input)
 st.code(user_input)
 #show the input in a json
 st.json(user_input)
+
+
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
