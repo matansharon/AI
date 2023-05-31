63a67e3085501994b19d6ad8b69f4f1cb87fb317
matan <matca2953@gmail.com>
2023-05-31 23:28:47 +0300

    in the middle of testing openai

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,22 +1,43 @@
 import streamlit as st
-st.write("Hello world!")
-#create an input line and show the input
-user_input = st.text_input("Please enter your name")
-st.write(user_input)
-#show the input in a header
-st.header(user_input)
-#show the input in a markdown
-st.markdown(user_input)
-#show the input in a code block
-st.code(user_input)
-#show the input in a json
-st.json(user_input)
+import openai
+import os
+from dotenv import load_dotenv
 
 
-import pandas as pd
+import os
+import openai
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+
+
+def get_query(question):
+    load_dotenv()
+    key = os.getenv("openai_key")
+    openai.api_key = key
+    response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=question,
+  temperature=0,
+  max_tokens=100,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
+  stop=["\n", "Q:"]
+)
+    return response.choices[0].text
+    
+#create a header that will as for a question. and a button to submit the question
+st.title("Ask a question")
+question = st.text_input("What is your question?")
+submit = st.button("Submit")
+
+#when the button is clicked, the question will be sent to the model and the answer will be returned
+if submit:
+    st.write("The answer to your question is: ")
+    q=get_query(question)
+    st.write(q)
+    print(q)
+    #get an answer from openai api gpt3 and display it
+    
