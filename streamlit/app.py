06c07326636d86ca5b0becffc64ca85aa4fff3c6
matansharon <matca2953@gmail.com>
2023-06-01 11:00:31 +0300

    from work pc

--- streamlit/app.py
+++ streamlit/app.py
@@ -18,17 +18,20 @@ def get_response(message):
     )
     return response.choices[0]["message"]["content"]
 
+def main():
+    #create a header that will as for a question. and a button to submit the question
+    st.title("Ask a question")
+    question = st.text_input("What is your question?")
+    submit = st.button("Submit")
 
-#create a header that will as for a question. and a button to submit the question
-st.title("Ask a question")
-question = st.text_input("What is your question?")
-submit = st.button("Submit")
-
-#when the button is clicked, the question will be sent to the model and the answer will be returned
-if submit:
-    st.write("The answer to your question is: ")
-    q=get_response(question)
-    st.write(q)
-    
-    #get an answer from openai api gpt3 and display it
-    
+    #when the button is clicked, the question will be sent to the model and the answer will be returned
+    if submit:
+        st.write("The answer to your question is: ")
+        q=get_response(question)
+        st.write(q)
+        
+        #get an answer from openai api gpt3 and display it
+        
+if __name__ == "__main__":
+    main()
+    # print('hello')
