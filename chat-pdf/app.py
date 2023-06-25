86d38a626d5e114abbc7a8af5c39809437e91e97
matan <matca2953@gmail.com>
2023-06-25 11:13:43 +0300

    working on app.py

--- chat-pdf/app.py
+++ chat-pdf/app.py
@@ -1,4 +1,16 @@
-import subprocess
+import os
+import streamlit as st
+from functions import *
+import openai
+import dotenv
+dotenv.load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# run a command
-subprocess.run(["pip list"], shell=True)
+
+def main():
+    st.title("Chat PDF")
+
+
+
+if __name__ == "__main__":
+    main()
--- chat-pdf/git_commands.py
+++ chat-pdf/git_commands.py
@@ -2,5 +2,5 @@ import subprocess
 
 # run a command
 subprocess.run(["git add --all"], shell=True)
-subprocess.run(["git commit -m 'chat-pdf in clone mode'"], shell=True)
+subprocess.run(["git commit -m 'working on app.py'"], shell=True)
 subprocess.run(["git push ai_github master"], shell=True)
