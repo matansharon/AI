44436f91f0c8625a0f90212f8e5c579543d71320
matan <matca2953@gmail.com>
2023-06-25 11:19:28 +0300

    working on app.py

--- chat-pdf/app.py
+++ chat-pdf/app.py
@@ -9,6 +9,11 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def main():
     st.title("Chat PDF")
+    uploadfile=st.file_uploader("Upload a PDF file", type=["pdf",'txt'])
+    if uploadfile is not None:
+        save_uploaded(uploadfile)
+        st.write("Please wait while we process your file...")
+        
 
 
 
