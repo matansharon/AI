32c5d57a25a3bf0d273917ac59b4c9410272a1e1
matan <matca2953@gmail.com>
2023-06-25 11:25:04 +0300

    working on app.py

--- chat-pdf/functions.py
+++ chat-pdf/functions.py
@@ -3,10 +3,11 @@ import PyPDF2
 
 
 def save_uploaded(uploadfile):
-    with open(uploadfile.filename, 'wb') as f:
+    with open(uploadfile, 'wb') as f:
         f.write(uploadfile.getbuffer())
 
 def learn_pdf(file_path):
     content_chunks=[]
     pdf_files=open(file_path,'rb')
-    pdf_reader=PyPDF2.PdfFileReader(pdf_files)
+    # pdf_reader=PyPDF2.PdfFileReader(pdf_files)
+    print(pdf_files)
