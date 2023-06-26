74ca288561de935a91e4420c0685ef5fccd9ca1d
matansharon <matca2953@gmail.com>
2023-06-26 06:43:42 +0300

    finish the backend of chat-pdf with liam

--- git_commands.py
+++ git_commands.py
@@ -4,5 +4,5 @@ import subprocess
 
 subprocess.run('git add --all', capture_output=True, text=True)
 
-subprocess.run('git commit -m "scraping a little bit"', capture_output=True, text=True)
+subprocess.run('git commit -m "finish the backend of chat-pdf with liam"', capture_output=True, text=True)
 subprocess.run('git push ai_github master', capture_output=True, text=True)
