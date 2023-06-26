9f97feedf0f81e3b2760d1b0be4a647d3427e562
matansharon <matca2953@gmail.com>
2023-06-26 14:45:25 +0300

    finish the backend of chat-pdf with liam

--- git_commands.py
+++ git_commands.py
@@ -1,8 +1,20 @@
 import subprocess
 
+import os
+
+# cmd1 = "git add --all"
+# cmd2= "git commit -m 'finish the backend of chat-pdf with liam'"
+# cmd3 = "git push ai_github master"
+# returned_value1 = os.system(cmd1)  # returns the exit code in unix
+# returned_value2 = os.system(cmd2)  # returns the exit code in unix
+# returned_value3 = os.system(cmd3)  # returns the exit code in unix
+
+# print('returned value:', returned_value1)
+# print('returned value:', returned_value2)
+# print('returned value:', returned_value3)
 # Run a Git command
 
-subprocess.run('git add --all', capture_output=True, text=True)
+subprocess.run('git add --all')
 
-subprocess.run('git commit -m "finish the backend of chat-pdf with liam"', capture_output=True, text=True)
-subprocess.run('git push ai_github master', capture_output=True, text=True)
+subprocess.run('git commit -m "finish the backend of chat-pdf with liam"')
+subprocess.run('git push ai_github master')
