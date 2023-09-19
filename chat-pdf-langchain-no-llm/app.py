4ca437405a23d6314e71dcfd9de70454a0a6ea72
matansharon <matca2953@gmail.com>
2023-09-19 15:34:51 +0300

    a lot of changes

--- chat-pdf-langchain-no-llm/app.py
+++ chat-pdf-langchain-no-llm/app.py
@@ -1,24 +1,51 @@
+from langchain.chains.summarize import load_summarize_chain
+from langchain.document_loaders import PyPDFLoader
+from langchain import OpenAI
 import streamlit as st
-import os
-import openai
-import pickle
-import langchain
 from dotenv import load_dotenv
+import os
+import tempfile
 
+# api_key=os.environ["OPENAI_API_KEY"]  
 
-def main():
-    st.header("Chat with PDF 💬")
-    st.sidebar.title('🤗💬 LLM Chat App')
-    st.sidebar.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [LangChain](https://python.langchain.com/)
-    - [OpenAI](https://platform.openai.com/docs/models) LLM model
+def summarize_pdfs_from_folder(pdfs_folder):
+    summaries = []
+    for pdf_file in pdfs_folder:
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_path = temp_file.name
+            temp_file.write(pdf_file.read())
+        
+        loader = PyPDFLoader(temp_path)
+        docs = loader.load_and_split()
+        chain = load_summarize_chain(llm, chain_type="map_reduce")
+        summary = chain.run(docs)
+        summaries.append(summary)
+
+        # Delete the temporary file
+        os.remove(temp_path)
     
-        ''')
+    return summaries
 
 
+if __name__ == "__main__":
+    # Load OpenAI API key
+    load_dotenv()
+    api_key=os.environ.get("openai_api_key")
+    # print(api_key)
+    llm = OpenAI(temperature=0)
 
-if __name__ == '__main__':
-    main()
+
+    # Streamlit App
+    st.title("Multiple PDF Summarizer")
+
+    # Allow user to upload PDF files
+    pdf_files = st.file_uploader("Upload PDF files", type="pdf", accept_multiple_files=True)
+
+    if pdf_files:
+        # Generate summaries when the "Generate Summary" button is clicked
+        if st.button("Generate Summary"):
+            st.write("Summaries:")
+            summaries = summarize_pdfs_from_folder(pdf_files)
+            for i, summary in enumerate(summaries):
+                st.write(f"Summary for PDF {i+1}:")
+                st.write(summary)
