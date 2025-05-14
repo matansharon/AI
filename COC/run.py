ce14cb7def1ff3c2f4c50d1a6075f1f2c598fbf4
matansharon <matca2953@gmail.com>
2025-05-14 22:17:25 +0300

    moving to production

--- COC/run.py
+++ COC/run.py
@@ -5,4 +5,4 @@ load_dotenv()
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8510)
---
+++ COC/wsgi.py
@@ -0,0 +1,11 @@
+from waitress import serve
+from run import app
+
+if __name__ == "__main__":
+    # Configure production server
+    serve(
+        app,
+        host="0.0.0.0",  # Listen on all available network interfaces
+        port=8080,       # You can change this port as needed
+        threads=4        # Adjust based on server capacity
+    )
